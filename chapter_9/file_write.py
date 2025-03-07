61f08cf858780b207688023119806d1d87998694
Umair Umar <umarumair622@gmail.com>
2025-03-07 15:28:59 +0500

    new python learning

---
+++ chapter_9/file_write.py
@@ -0,0 +1,8 @@
+st = "Hey Umair you are amazing"
+
+
+f = open("myfile.txt", "w")
+
+f.write(st)
+
+f.close()
---
+++ chapter_9/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+line1 = f.readline()
+# print(line1, type(line1))
+
+# line2 = f.readline()
+# print(line2, type(line2))
+
+# line2 = f.readline()
+# print(line2, type(line2))
+
+# line3 = f.readline()
+# print(line3, type(line3))
+
+# line4 = f.readline()
+# print(line4, type(line4))
